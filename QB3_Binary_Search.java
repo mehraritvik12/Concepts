02403b657084e9fc187f67ad92a06ee26f19a17a
mehraritvik12 <112796687+mehraritvik12@users.noreply.github.com>
2023-11-04 22:26:03 +0530

    Add files via upload

--- QB3_Binary_Search.java
+++ QB3_Binary_Search.java
@@ -7,29 +7,82 @@ public class QB3_Binary_Search {
 
     public static int BinarySearch(int[] arr, int target) {
 
+
         int start = 0;
         int end = arr.length - 1;
 
-        while (start <= end) {
-            int mid = (end - start) / 2;
 
-            if (target < arr[mid]) {
-                end = mid - 1;
-            }
-            else if (target > arr[mid]) {
-                start = mid + 1;
-            }
-            else{
+        // find arr is ascending or descending
+
+        boolean isAsc = arr[start] < arr[end];
+
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+        //same case for both
+            if (target == arr[mid]) {
                 return mid;
             }
+
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
         }
+
         return -1;
     }
+//    if(arr[0]<arr[1]) {
+//        int start = 0;
+//        int end = arr.length - 1;
+//
+//        while (start <= end) {
+//            int mid = start + (end - start) / 2;
+//
+//            if (target < arr[mid]) {
+//                end = mid - 1;
+//            } else if (target > arr[mid]) {
+//                start = mid + 1;
+//            } else {
+//                return mid;
+//            }
+//        }
+//    }
+//    else{
+//        int start = 0;
+//        int end = arr.length - 1;
+//
+//        while (start <= end) {
+//            int mid = start + (end - start) / 2;
+//
+//            if (target > arr[mid]) {
+//                end = mid - 1;
+//            } else if (target < arr[mid]) {
+//                start = mid + 1;
+//            } else {
+//                return mid;
+//            }
+//        }
+//
+//    }
+//        return -1;
+//    }
 
     public static void main(String[] args) {
 
 
-        int arr[] = {1, 2, 3, 3, 4, 2, 4, 5, 7, 8, 9, 65, 4, 332, 2, 2, 44, 5,};
-       System.out.println(BinarySearch(arr,65));
+        int arr[] = {9,7,5,4,3,2,1};
+       System.out.println(BinarySearch(arr,9));
     }
 }
