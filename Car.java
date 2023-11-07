4e9d1964fbe0b17a2cee318f11df88b91174bd68
mehraritvik12 <112796687+mehraritvik12@users.noreply.github.com>
2023-11-07 22:44:05 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,16 @@
+package assignment;
+
+public class Car {
+	void print(){
+	    System.out.print("Car class ");
+	}
+	    
+	    public static void main(String[] args){   
+	        Car obj1 = new Car();
+	        obj1.print();
+
+	        Vehicles obj2 = new Vehicles();
+	        obj2.print();  
+	       
+}
+}
